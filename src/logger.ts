d4b84e57130d330f21893a36437c52a8ed83b777
Dafeng Guo <dfguo.joe@gmail.com>
2018-01-27 16:06:10 +0800

    tweak some text

--- src/logger.ts
+++ src/logger.ts
@@ -1,75 +1,81 @@
-import * as fs from "fs";
-import * as chalk from "chalk";
-import { Store, Version } from "./parser";
-import { Output } from "./output";
-const readline = require("readline");
-const o = new Output();
-const log = console.log;
+import * as fs from 'fs'
+import * as chalk from 'chalk'
+import { Store, Version } from './parser'
+import { Output } from './output'
+const readline = require('readline')
+const o = new Output()
+const log = console.log
 
 interface LoggerOptions {
-  insure: boolean;
-  intro: boolean;
+  insure: boolean
+  intro: boolean
 }
 export class Logger {
-  constructor(private loggerPath: string, private store: Store, private options: LoggerOptions) {}
+  constructor(
+    private loggerPath: string,
+    private store: Store,
+    private options: LoggerOptions
+  ) {}
 
   private getFreshVersions() {
-    const title = this.findLoggerFile() ? fs.readFileSync(this.loggerPath).toString() : "";
-    return this.store.getFreshVersions(title);
+    const title = this.findLoggerFile()
+      ? fs.readFileSync(this.loggerPath).toString()
+      : ''
+    return this.store.getFreshVersions(title)
   }
 
   private findLoggerFile(): boolean {
-    return fs.existsSync(this.loggerPath);
+    return fs.existsSync(this.loggerPath)
   }
 
   private wrtieLoggerFile(): void {
-    const latestVersion = this.store.getLatestVersion();
-    fs.writeFileSync(this.loggerPath, latestVersion.title);
+    const latestVersion = this.store.getLatestVersion()
+    fs.writeFileSync(this.loggerPath, latestVersion.title)
   }
 
   public log() {
-    const freshVersions = this.getFreshVersions();
+    const freshVersions = this.getFreshVersions()
     // If no fresh versions, it's unnecessary to enquire.
     if (freshVersions.length === 0) {
-      o.showNochange();
+      o.showNochange()
     }
-    this.options.intro && this.displayIntro();
+    this.options.intro && this.displayIntro()
     freshVersions.forEach(version => {
-      this.displayVersion(version);
-    });
-    
+      this.displayVersion(version)
+    })
+
     if (freshVersions.length > 0) {
       // If answer is Y, record this current version
-      this.options.insure && this.inquiry();
+      this.options.insure && this.inquiry()
     }
   }
 
   private displayIntro() {
-    o.showIntro(this.store.intro);
+    o.showIntro(this.store.intro)
   }
 
   private inquiry() {
     const rl = readline.createInterface({
       input: process.stdin,
-      output: process.stdout
-    });
-    rl.question("Do you already know all the changes? (Y/n) ", answer => {
-      if (answer != "Y") {
-        this.log();
-        return false;
+      output: process.stdout,
+    })
+    rl.question('Changes acknowledged (Y/n) ', answer => {
+      if (answer != 'Y') {
+        this.log()
+        return false
       } else {
-        this.wrtieLoggerFile();
-        rl.close();
-        return true;
+        this.wrtieLoggerFile()
+        rl.close()
+        return true
       }
-    });
+    })
   }
 
   private displayVersion(version: Version) {
-    o.addVersion(version.title);
+    o.addVersion(version.title)
     version.changes.forEach(change => {
-      o.addChange(change.type, change.items);
-    });
-    o.show();
+      o.addChange(change.type, change.items)
+    })
+    o.show()
   }
 }
